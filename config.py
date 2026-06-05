9343a73202ae0b54be4e5045f7613eafb17fbcf0
AdyStyLe <barbeariamnpro@gmail.com>
2026-06-05 14:51:00 +0100

    Simplify database URI configuration

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua-chave-secreta-aqui'
     
     # PostgreSQL com Supabase
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:SUA_SENHA@db.rwwwtgeefkqqagqalfzz.supabase.co:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
